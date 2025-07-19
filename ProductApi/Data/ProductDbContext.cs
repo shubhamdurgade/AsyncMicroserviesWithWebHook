c1245ff7697a0d5b0a125d604272696888f3169a
Shubham Durgade <shubhamdurgade1203@gmail.com>
2025-07-19 19:02:45 +0530

    Add code for first migration and update database

---
+++ ProductApi/Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Model;
+
+namespace ProductApi.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
--- Shared/Model/Product.cs
+++ Shared/Model/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace Shared.Model
 {
     public class Product
     {
-        
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
     }
 }
